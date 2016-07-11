46cd43d4b268a41125b396263822b2870e65672c
alomina007 <alomina007@users.noreply.github.com>
2016-07-11 20:54:58 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {208094932,126355654},--Sudo users
+    sudo_users = {208094932},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[alomina V.1
